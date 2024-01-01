ae31ccafb5849eec9897e0b49db8b4f392950dc5
joshuawynn <joshua.a.wynn@gmail.com>
2024-01-01 16:26:03 -0500

    update schedule controller

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -14,9 +14,9 @@ async function deleteTask(req, res) {
 async function create(req, res) {
   const member = await Member.findById(req.params.id);
   // Add the user-centric info to req.body (the new review)
-    //  req.body.user = req.user._id;
-    //  req.body.userName = req.user.name;
-    //  req.body.userAvatar = req.user.avatar;
+     req.body.user = req.user._id;
+     req.body.userName = req.user.name;
+     req.body.userAvatar = req.user.avatar;
 
   // We can push (or unshift) subdocs into Mongoose arrays
   member.tasks.push(req.body);
